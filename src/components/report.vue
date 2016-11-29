69d796a74d9056b08473b23c16e69110a931e110
fangbao <1197036595@qq.com>
2016-11-29 18:19:24 +0800

    update report.vue

--- src/components/report.vue
+++ src/components/report.vue
@@ -2,11 +2,18 @@
 	<div v-show="isshow" class="report-bg" @click="cancel">
 		<div class="report-main" @click.stop="">
 			<div class="report-head">
-				<span class="cancel">取消</span>
-				<span class="send">发表</span>
+				<span class="send">举报内容问题</span>
 			</div>
 			<div class="content">
-				<textarea autofocus="autofocus"></textarea>
+					<ul>
+						<li v-for="(item,index) in items" v-on:click="checked(index)" :class="{'gray':item.checked}">
+							 <span ><em v-if="item.checked" >已举报  </em>{{item.title}}</span>
+						     <i  v-if="item.checked" class="iconfont">&#xe670;</i>
+						</li>
+					</ul>
+				    <div class="report">
+						<el-button type="primary">提交</el-button>
+					</div>
 			</div>
 		</div>
 	</div>
@@ -16,20 +23,55 @@
 		props:['isshow'],
 		data(){
 			return {
-				
-			}
+				items:[
+					{
+						"title":"广告",
+						"checked":false
+
+					},{
+						"title":"重复、旧闻",
+						"checked":false
+					},{
+						"title":"格式问题",
+			             "checked":false
+					},{
+						"title":"低俗",
+						"checked":false
+					}, {
+						"title":"标题夸张",
+						"checked":false
+					},{
+						"title":"已举报与事实不符",
+						"checked":false
+					},{
+						"title":"疑似抄袭",
+						"checked":false
+					},{
+						"title":"其他问题",
+						"checked":false
+					}
+				]
+			};
+
 		},
 		mounted(){
 			 
 			
 		},
 		methods:{
+			checked(index){
+				this.items[index].checked = !this.items[index].checked;
+			},
 			cancel(){
 				this.$parent.isshow = false;
 			},
 		}
 	}
 </script>
+
+
+
+
 <style lang="sass" scoped>
 	.report-bg{
 		width: 6.4rem;
@@ -47,17 +89,12 @@
 			line-height: .6rem;
 			width:90%;
 			height:.6rem;
+			text-align: center;
 			background-color: #F7F7F7;
 			padding:0 5%;
-			span{
-				font-size:.26rem;
-			}
-			.cancel{
-				color:#000;
-			}
 			.send{
 				color:#6699FF;
-				float:right;
+				font-size: .28rem;
 			}
 		}
 		.content{
@@ -70,6 +107,25 @@
 				border:none;
 				min-height: 2rem;
 			}
+			li{
+				padding:.15rem 0;
+				border-bottom: 1px solid #e8e8e8;
+			}
+			.gray{
+				color:gray;
+			}
+			span{
+				font-size: .24rem;
+			}
+			i{
+				color:#6699FF;
+				float: right;
+			}
+			.report{
+				text-align: center;
+				padding: .3rem 0;
+			}
+
 		}
 	}
 </style>
